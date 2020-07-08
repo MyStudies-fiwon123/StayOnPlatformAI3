89557d718e298cbacc25cfebb6c0badde910ded3
fiwon123 <felipeinoue@yahoo.com.br>
2020-07-08 19:24:12 -0300

    Finished

--- Assets/Brain.cs
+++ Assets/Brain.cs
@@ -6,15 +6,29 @@ public class Brain : MonoBehaviour
 {
     int DNALength = 2;
     public float timeAlive;
+    public float timeWalking;
     public DNA dna;
     public GameObject eyes;
     bool alive = true;
     bool seeGround = true;
 
+    public GameObject ethanPrefab;
+    GameObject ethan;
+
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed.
+    /// </summary>
+    void OnDestroy()
+    {
+        Destroy(ethan);
+    }
+
     void OnCollisionEnter(Collision obj)
     {
         if (obj.gameObject.tag == "dead"){
             alive = false;
+            timeAlive = 0;
+            timeWalking = 0;
         }
     }
 
@@ -27,6 +41,8 @@ public class Brain : MonoBehaviour
         dna = new DNA(DNALength, 3);
         timeAlive = 0;
         alive = true;
+        ethan = Instantiate(ethanPrefab, this.transform.position, this.transform.rotation);
+        ethan.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControl>().target = this.transform;
     }
 
     /// <summary>
@@ -38,24 +54,24 @@ public class Brain : MonoBehaviour
 
         Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 10, Color.red, 10);
         seeGround = false;
-        RaycasHit hit;
+        RaycastHit hit;
         if (Physics.Raycast(eyes.transform.position, eyes.transform.forward * 10, out hit)){
             if (hit.collider.gameObject.tag == "platform"){
                 seeGround = true;
             }
         }
-        timeAlive = populationManager.elapsed;
+        timeAlive = PopulationManager.elapsed;
 
         // read DNA
         float turn = 0;
         float move = 0;
         if (seeGround){
             // make v relative to character and always move forward
-            if (dna.GetGene(0) == 0) move = 1;
+            if (dna.GetGene(0) == 0) {move = 1; timeWalking += 1;}
             else if (dna.GetGene(0) == 1) turn = -90;
             else if (dna.GetGene(0) == 2) turn = 90;
         } else {
-            if (dna.GetGene(1) == 0) move = 1;
+            if (dna.GetGene(1) == 0) {move = 1; timeWalking += 1;}
             else if (dna.GetGene(1) == 1) turn = -90;
             else if (dna.GetGene(1) == 2) turn = 90;
         }
